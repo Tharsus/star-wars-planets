4457ba3a82a940cab4d1438de5f8fc071c0a0b45
tharsus.proux <tharsus@poli.ufrj.br>
2020-11-28 15:37:59 -0300

    Included first endpoing retrieve all planets

--- controllers/planetController.js
+++ controllers/planetController.js
@@ -1,5 +1,19 @@
+import { db } from '../models/dbModel.js';
+
 const retrieve = async (req, res) => {
-  console.log('planet');
+  try {
+    const dbPlanets = await db.model.find();
+
+    if (dbPlanets.length < 1) {
+      res.status(404).send({ message: "Couldn't find any planets" });
+    }
+
+    res.send(dbPlanets);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Error while retrieving planets' });
+  }
 };
 
 export default { retrieve };
